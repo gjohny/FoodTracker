762c7b571446ed7a9439f3bd7cf454661a61c921
gjohny <githinjohny@gmail.com>
2024-08-19 12:04:14 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from website import create_app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True) #automatically rerun the web server when a change is made (turn off in production)
