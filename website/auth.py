759c801f6267867449646da5afa10207e0b67090
gjohny <githinjohny@gmail.com>
2024-08-21 23:49:49 -0500

    Fixed DataError: value type too long

--- website/auth.py
+++ website/auth.py
@@ -52,7 +52,8 @@ def sign_up():
         elif len(password1) <= 7:
             flash('Password must be at least 8 characters.', category='warning')
         else:
-            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='scrypt'))
+            pw = generate_password_hash(password1, method='pbkdf2', salt_length=16)
+            new_user = User(email=email, first_name=first_name, password=pw)
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user)
