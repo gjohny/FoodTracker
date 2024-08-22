29094dc07d39481458b84144f0905de0968e3de1
gjohny <githinjohny@gmail.com>
2024-08-21 23:32:54 -0500

    modified string length

--- website/models.py
+++ website/models.py
@@ -5,16 +5,16 @@ from datetime import datetime
 
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    data = db.Column(db.String)
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True)
-    password = db.Column(db.String(150))
-    first_name = db.Column(db.String(150))
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    first_name = db.Column(db.String)
     date_joined = db.Column(db.DateTime(timezone=True), default=func.now())
     height = db.Column(db.Integer)
     weight = db.Column(db.Integer)
@@ -29,17 +29,17 @@ class Meal(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     calories = db.Column(db.Integer)
-    protien = db.Column(db.String(100))
-    carbs = db.Column(db.String(100))
-    fat = db.Column(db.String(100))
+    protien = db.Column(db.String)
+    carbs = db.Column(db.String)
+    fat = db.Column(db.String)
     # foods = db.relationship('Food')
 
 class Food(db.Model):
     __tablename__ = "food"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    name = db.Column(db.String(100))
-    calories = db.Column(db.String(100))
+    name = db.Column(db.String)
+    calories = db.Column(db.String)
     protien = db.Column(db.Integer)
     carbs = db.Column(db.Integer)
     fat = db.Column(db.Integer)
