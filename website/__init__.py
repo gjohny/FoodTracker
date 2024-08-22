9a33d9f2723d2f4cab837e4aba61ba7c936e91bf
gjohny <githinjohny@gmail.com>
2024-08-21 23:03:07 -0500

    imported db config

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from db_config import DB
+from .db_config import DB
 
 
 db = SQLAlchemy()
