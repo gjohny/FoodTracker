6bfc03473c494b125eaa91610137a77747cb6c8a
gjohny <githinjohny@gmail.com>
2024-08-21 23:14:25 -0500

    direct sql link, remove extra modue2

--- website/config.py
+++ website/config.py
@@ -1,6 +1,6 @@
-import os
+# import os
 
 
-class config:
-    SECRET_KEY = os.getenv('secret_user_key', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'database.db')
+# class config:
+#     SECRET_KEY = os.getenv('secret_user_key', os.urandom(24))
+#     SQLALCHEMY_DATABASE_URI = os.getenv('Database_URL', 'database.db')
---
+++ website/db_config.py
@@ -0,0 +1,1 @@
+# DB = 'postgres://default:jJx2GPQWH9Yg@ep-silent-butterfly-a4hqfavj-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require'
