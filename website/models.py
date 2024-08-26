a9afec9e7356713789abefc13736f29d4c917d37
gjohny <githinjohny@gmail.com>
2024-08-26 10:42:01 -0500

    location modules error fixed <NOTE>

--- website/models.py
+++ website/models.py
@@ -3,11 +3,11 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 from datetime import datetime
 
-# class Note(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     data = db.Column(db.String)
-#     date = db.Column(db.DateTime(timezone=True), default=func.now())
-#     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class User(db.Model, UserMixin):
